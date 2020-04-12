9e4a4c1f16081bab7870c331aef08695462f9361
Clark <thecluskin@gmail.com>
2020-04-12 11:56:03 -0400

    removing graphql calls during ssr to see if that fixed the 500 error on the server

--- web-client/src/graphql/withApollo.js
+++ web-client/src/graphql/withApollo.js
@@ -1,52 +1,13 @@
 import React from "react";
 import { initApollo } from "./initApollo";
-import Head from "next/head";
-import { getDataFromTree } from "@apollo/react-ssr";
 
 export const withApollo = (App) => {
   return class Apollo extends React.Component {
     static displayName = "withApollo(App)";
-    static async getInitialProps(ctx) {
-      const { AppTree } = ctx;
-
-      let appProps = {};
-      if (App.getInitialProps) {
-        appProps = await App.getInitialProps(ctx);
-      }
-
-      // Run all GraphQL queries in the component tree
-      // and extract the resulting data
-      const apollo = initApollo();
-      if (!process.browser) {
-        try {
-          // Run all GraphQL queries
-          await getDataFromTree(
-            <AppTree {...appProps} apolloClient={apollo} />
-          );
-        } catch (error) {
-          // Prevent Apollo Client GraphQL errors from crashing SSR.
-          // Handle them in components via the data.error prop:
-          // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
-          console.error("Error while running `getDataFromTree`", error);
-        }
-
-        // getDataFromTree does not call componentWillUnmount
-        // head side effect therefore need to be cleared manually
-        Head.rewind();
-      }
-
-      // Extract query data from the Apollo store
-      const apolloState = apollo.cache.extract();
-
-      return {
-        ...appProps,
-        apolloState,
-      };
-    }
 
     constructor(props) {
       super(props);
-      this.apolloClient = initApollo(props.apolloState);
+      this.apolloClient = initApollo();
     }
 
     render() {
