1d271f325c06743eb60fcf5d39bbf3b7726956a9
Clark <thecluskin@gmail.com>
2020-04-22 00:28:46 -0400

    added match domain to app can create matches in db
    removed console.log calls

--- src/state/redux/home/middleware/navigateToRoom.js
+++ src/state/redux/home/middleware/navigateToRoom.js
@@ -3,10 +3,8 @@ import { openRoomsTableRowClicked } from "../actions";
 
 export const navigateToRoom = () => (next) => (action) => {
   next(action);
-  console.log("called")
 
   if (action.type === openRoomsTableRowClicked.type) {
-    console.log("nav to room")
     Router.push(`/room/${action.payload.roomId}`);
   }
 };
