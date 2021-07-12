170b8648eec699afb28ff3d309cbe3a0f2bccf05
Clark <thecluskin@gmail.com>
2021-07-11 22:50:14 -0400

    added session to auth reducer

--- src/state/redux/auth/reducer.ts
+++ src/state/redux/auth/reducer.ts
@@ -1,5 +1,5 @@
 import ReducerBuilder from "state/redux/utils/factories/ReducerBuilder";
-import { userReceived, profileReceived } from "./actions";
+import { userReceived, profileReceived, sessionReceived } from "./actions";
 import { setKeyToPayload } from "state/redux/utils/reducers/setKeyToPayload";
 
 export const reducer = new ReducerBuilder()
@@ -8,4 +8,8 @@ export const reducer = new ReducerBuilder()
     user,
   }))
   .addReducer(profileReceived.type, setKeyToPayload({ key: "profile" }))
+  .addReducer(sessionReceived.type, (state, { payload }) => ({
+    ...state,
+    session: payload?.session,
+  }))
   .build();
